8d511778c5201831a695a7c7046cac0004124b2c
ivan <ivan1900@gmail.com>
2021-01-14 22:12:44 +0100

    new value object Uuid

---
+++ app/Src/shared/domain/valueObject/Uuid.php
@@ -0,0 +1,38 @@
+<?php namespace App\Src\shared\domain\valueObject;
+
+use InvalidArgumentException;
+use Ramsey\Uuid\Uuid as RamseyUuid;
+
+final class Uuid
+{
+    protected string $value;
+
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    public function random():self
+    {
+        return new static(RamseyUuid::uuid4()->toString());
+    }
+    
+    public function value():string
+    {
+        return $this->value();
+    }
+
+    public function equals(Uuid $anUuid): bool
+    {
+        return $this->value() === $anUuid->value();
+    }
+
+    //aqui __tostring cuanto actualice a php 8
+
+    public function ensureIsValidUuid(string $id)
+    {
+        if(!RamseyUuid::isValid($id)){
+            throw new InvalidArgumentException(sprintf('<%s> does not allow the value <%s>.', static::class, $id));
+        }
+    }
+}
