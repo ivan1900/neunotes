df8cc88331326cea98da54ed456807a1b372d6e6
ivan <ivan1900@gmail.com>
2021-02-28 15:36:30 +0100

    move store to shared folder

--- app/Views/footers/footer_users.php
+++ app/Views/footers/footer_users.php
@@ -15,7 +15,7 @@
 <script src="<?php echo base_url();?>/assets/js/vue-tables-2.min.js"></script>
 
 <script src="<?php echo base_url();?>/assets/js/users/vueTables2.js"></script>
-<script src="<?php echo base_url();?>/assets/js/users/store.js"></script>
+<script src="<?php echo base_url();?>/assets/js/shared/store.js"></script>
 
 <script src="<?php echo base_url();?>/assets/js/users/vueTableUsers.js"></script>
 <script src="<?php echo base_url();?>/assets/js/users/vueUsers.js"></script>
