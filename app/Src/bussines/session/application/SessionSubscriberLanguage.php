b5712ded069e13a1b474b7714d0dd55543e7c4d0
ivan <ivan1900@gmail.com>
2021-09-28 22:11:25 +0200

    Remove obsolet controllers and module session

--- app/Src/bussines/session/application/SessionSubscriberLanguage.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App\Src\bussines\session\application;
-
-class SessionSubscriberLanguage
-{
-    static public function handle($language)
-    {
-        $_SESSION['language']=$language;
-    }
-}
