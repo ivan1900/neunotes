21dd0d9969546af7bdbe5b2a9c7e4fb984968575
ivan <ivan1900@gmail.com>
2021-10-06 22:42:36 +0200

    working in endpoint create note

--- app/Controllers/NotesResf.php
+++ app/Controllers/NotesResf.php
@@ -1,10 +1,30 @@
 <?php namespace App\Controllers;
 
 use CodeIgniter\RESTful\ResourceController;
+use App\Src\bussines\notes\application\CreateNote;
+use App\Src\bussines\notes\application\RequestCreateNote;
 
 class NotesResf extends ResourceController
 {
     protected $format = 'json';
+
+    public function create()
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'OPTIONS') {
+            $requestCreateNote = new RequestCreateNote(
+                $title: $_POST['title'],
+                $content: $_POST['content']
+            );
+            $createNote = new CreateNote();
+
+            try
+            {
+                $createNote->create($requestCreateNote);        
+            }catch(\Exception){
+
+            }
+        }
+    }
 
     
 }
